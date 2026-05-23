716cfad7189cfa6a4f92e3e351c34db854df3527
Dylan Tang <dtang04@uchicago.edu>
2026-05-23 16:44:03 -0500

    refactor to improve fault tolerance

--- client/helpers.py
+++ client/helpers.py
@@ -1,5 +1,7 @@
 import requests
 
+from errors import IDCreationError, GetHistoryError, RegisterUsernameError, RecipientIDError
+
 HTTP_URI = "http://localhost:8000"
 
 def getHistory(c_uname, r_uname):
@@ -10,3 +12,31 @@ def getHistory(c_uname, r_uname):
         hist = resp["history"]
         for text in hist:
             print(f"[From: {text['from']} @ {text['ts']}] {text['msg']}")
+        return
+        
+    raise GetHistoryError("History retrieval failed")
+
+def getRecipientID(r_username):
+    resp = requests.get(HTTP_URI + f"/id/{r_username}")
+    
+    if resp.status_code == 200:
+        resp = resp.json()
+        return resp["r_id"]
+    
+    raise RecipientIDError("Recipient not found")
+
+def generate_id():
+    resp = requests.get(HTTP_URI + "/id")
+    if resp.status_code == 200:
+        resp = resp.json() 
+        return resp["c_id"]
+    
+    raise IDCreationError("ID generation failed")
+
+def registerUsername(username, user_id):
+    resp = requests.post(HTTP_URI + "/username", json={"username": username, "userid": user_id})
+    if resp.status_code == 200:
+        return
+    
+    raise RegisterUsernameError("Serverside username registration failed")
+
